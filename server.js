0bc483cffcab21966c8bea2a5deb8d3f3fc4aa4b
Jeremy Crafts <jcrafts@redhat.com>
2017-11-15 14:12:37 -0500

    forward GET req to API

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const request = require('request');
 const pugToHtml = require('./pugToHtml');
 
 const config = require('./config');
@@ -14,6 +15,12 @@ const scriptsDir = path.join(config.appRoot, 'scripts');
 app.set('port', config.port);
 app.use(express.static(publicDir));
 app.use('/scripts', express.static(scriptsDir));
+
+app.get('/report/:id', (req, res) => {
+    request
+    .get(process.env.BIGZAM_API + '/report/' + req.params.id)
+    .pipe(res);
+});
 
 // initialize~
 pugToHtml(templateDir, publicDir);
